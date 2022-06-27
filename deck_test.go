14cd5097dac4caa030d484f9215b71d9ab8e7cb1
Ahan Jaiswal <63398617+maugus0@users.noreply.github.com>
2022-06-27 11:08:20 +0530

    Adding source code.

---
+++ deck_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected Deck Length of 52, but got:", len(d))
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module shuffleX
+
+go 1.18
